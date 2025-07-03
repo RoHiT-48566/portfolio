a7dc288e1d32e14a704b1c5c2c0191b6c5ded3b9
RoHiT-48566 <m.rohit48566@gmail.com>
2025-07-04 00:07:14 +0530

    Contact, Projects & Footer section completed.

---
+++ app/components/Footer.jsx
@@ -0,0 +1,34 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image src={assets.logo} alt="logo" className="w-36 mx-auto mb-2" />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image src={assets.mail_icon} alt="logo" className="w-6" />
+          m.rohit48566@gmail.com
+        </div>
+      </div>
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>© 2025 Rohit Mathi. All rights reserved.</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/RoHiT-48566">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/RoHiT-48566">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
