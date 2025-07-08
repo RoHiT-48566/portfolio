870ffdf560de8f451c5433d3d185ea2af01943b9
RoHiT-48566 <m.rohit48566@gmail.com>
2025-07-09 01:49:51 +0530

    Layout and other important features updated.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 
-const Navbar = () => {
+const Navbar = ({ isDarkMode, setIsDarkMode }) => {
   const [isScrolled, setIsScrolled] = useState(false);
   const sideMenuRef = useRef();
   const openMenu = () => {
@@ -22,7 +22,7 @@ const Navbar = () => {
   }, []);
   return (
     <>
-      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
         <Image
           src={assets.header_bg_color}
           alt="header-bg"
@@ -31,19 +31,23 @@ const Navbar = () => {
       </div>
       <nav
         className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
-          isScrolled ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+          isScrolled
+            ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20"
+            : ""
         }`}
       >
-        <a href="#top">
+        <a href="#top" className="inline-block">
           <Image
-            src={assets.logo}
+            src={isDarkMode ? assets.logo_dark : assets.logo}
             alt="logo"
-            className="w-28 cursor-pointer mr-14"
+            className="w-28 cursor-pointer mr-auto"
           />
         </a>
         <ul
           className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
-            isScrolled ? "" : "bg-white shadow-sm bg-opacity-50"
+            isScrolled
+              ? ""
+              : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"
           }`}
         >
           <li>
@@ -53,49 +57,45 @@ const Navbar = () => {
           </li>
           <li>
             <a className="font-Ovo" href="#about">
-              About me
-            </a>
-          </li>
-          <li>
-            <a className="font-Ovo" href="#services">
-              Services
+              About Me
             </a>
           </li>
           <li>
             <a className="font-Ovo" href="#work">
-              My Work
+              Projects
             </a>
           </li>
           <li>
             <a className="font-Ovo" href="#contact">
-              Contact me
+              Contact
             </a>
           </li>
         </ul>
         <div className="flex items-center gap-4">
-          <button>
-            <Image src={assets.moon_icon} alt="moon-toggle" className="w-6" />
+          <button onClick={() => setIsDarkMode((prev) => !prev)}>
+            <Image
+              src={isDarkMode ? assets.sun_icon : assets.moon_icon}
+              alt="moon-toggle"
+              className="w-6"
+            />
           </button>
-          <a
-            href="#contact"
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
-          >
-            Contact{" "}
-            <Image src={assets.arrow_icon} alt="contact" className="w-3" />
-          </a>
           <button className="block md:hidden ml-3" onClick={openMenu}>
-            <Image src={assets.menu_black} alt="menu-black" className="w-6" />
+            <Image
+              src={isDarkMode ? assets.menu_white : assets.menu_black}
+              alt="menu-black"
+              className="w-6"
+            />
           </button>
         </div>
 
         {/* Mobile Menu */}
         <ul
           ref={sideMenuRef}
-          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white"
         >
           <div className="absolute right-6 top-6" onClick={closeMenu}>
             <Image
-              src={assets.close_black}
+              src={isDarkMode ? assets.close_white : assets.close_black}
               alt="close"
               className="w-5 cursor-pointer"
             />
@@ -112,18 +112,13 @@ const Navbar = () => {
             </a>
           </li>
           <li>
-            <a onClick={closeMenu} className="font-Ovo" href="#services">
-              Services
-            </a>
-          </li>
-          <li>
             <a onClick={closeMenu} className="font-Ovo" href="#work">
               My Work
             </a>
           </li>
           <li>
             <a onClick={closeMenu} className="font-Ovo" href="#contact">
-              Contact me
+              Contact
             </a>
           </li>
         </ul>
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,21 +1,56 @@
 import { assets, workData } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { motion } from "motion/react";
 
-const Work = () => {
+const Work = ({ isDarkMode }) => {
   return (
-    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
-      <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
-      <p className="text0-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="work"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        My Latest Works
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        Projects
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+      >
         Welcome to my web development portfolio! Explore a collection of
-        projects showcasing my expertise in front-end development.
-      </p>
-      <div className="grid grid-cols-auto my-10 gap-5">
+        projects that demonstrate my skills in building full-stack applications,
+        combining both intuitive front-end interfaces and robust back-end
+        systems.
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.9 }}
+        className="grid grid-cols-auto my-10 gap-5 dark:text-black"
+      >
         {workData.map((project, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
             key={index}
-            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group border-2 border-black-200"
             style={{ backgroundImage: `url(${project.bgImage})` }}
           >
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
@@ -24,25 +59,35 @@ const Work = () => {
                 <p className="text-sm text-gray-700">{project.description}</p>
               </div>
               <div className="border rounded-full border-black w-9 aspect-square flex items-center js=ustify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-                <Image src={assets.send_icon} alt="send icon" className="w-5" />
+                <Image
+                  src={assets.send_icon}
+                  alt="send icon"
+                  className="w-5"
+                  onClick={() => window.open(project.link, "_blank")}
+                />
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
-      <a
+      </motion.div>
+      <motion.a
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1.1 }}
         href="https://github.com/RoHiT-48566"
         target="_blank"
-        className="w-max flex items-center justify-center gap-2 test-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500"
+        className="w-max flex items-center justify-center gap-2 test-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover"
       >
         Show more{" "}
         <Image
-          src={assets.right_arrow_bold}
+          src={
+            isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold
+          }
           alt="right arrow"
           className="w-4"
         />
-      </a>
-    </div>
+      </motion.a>
+    </motion.div>
   );
 };
 
